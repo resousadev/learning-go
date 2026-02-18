7ffc7a1aa8b5d4e511c2a655ec0a46db5e07cf0e
resousadev <resousadev@gmail.com>
2026-02-17 22:34:22 -0300

    feat: Assigning values from three variables into a single variable

--- level_001/main.go
+++ level_001/main.go
@@ -8,9 +8,12 @@ var z bool
 
 func main() {
 
-	// Aqui será exibido o valor zero de cada variável, pois elas não foram inicializadas
-	fmt.Printf("O valor de x é: %#v\n", x)
-	fmt.Printf("O valor de y é: %#v\n", y)
-	fmt.Printf("O valor de z é: %#v\n", z)
+	x = 42
+	y = "Go is awesome!"
+	z = true
+
+	// Aqui estamos atribuindo todos os valores a uma string formatada usando Sprintf
+	s := fmt.Sprintf("%d %s %t", x, y, z)
+	fmt.Println(s)
 
 }
