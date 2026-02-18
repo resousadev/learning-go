703950f8d16616a0a8bac99f6be64ce428137666
resousadev <resousadev@gmail.com>
2026-02-17 22:56:39 -0300

    feat: Change variable x to int and update y assignment and print statements

--- level_001/main.go
+++ level_001/main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 type meu_tipo int
 
-var x meu_tipo
+var x int
+var y meu_tipo
 
 func main() {
 
@@ -15,5 +16,11 @@ func main() {
 	x = 42
 
 	fmt.Printf("O valor de x é: %v\n", x)
+
+	y = meu_tipo(x)
+
+	fmt.Printf("O valor de y é: %v\n", y)
+
+	fmt.Printf("O tipo de y é: %T\n", y)
 
 }
